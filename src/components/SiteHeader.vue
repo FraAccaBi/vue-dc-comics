e0184769906ae098a9e8e66d1446b03f411f00ff
FraAccaBi <francescahuda@gmail.com>
2022-05-03 17:31:33 +0200

    backup commit

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <header class="container-fluid d-flex">
+            <img class="" src="../assets/img/dc-logo.png" alt="">
+            <nav class="d-flex">
+                <ul>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>Tv</li>
+                    <li>Games</li>
+                    <li>Collectibles</li>
+                    <li>Videos</li>
+                    <li>Fans</li>
+                    <li>News</li>
+                    <li>Shop</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SiteHeader',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
