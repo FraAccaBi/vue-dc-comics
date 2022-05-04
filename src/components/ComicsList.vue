f1984bcd7dba03984dc91a891ebb8eb2bef25b0b
FraAccaBi <francescahuda@gmail.com>
2022-05-04 19:14:10 +0200

    backup commit

---
+++ src/components/ComicsList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="col">
+        <div class="series_img">
+            <img :src="thumb">
+        </div>
+        <div class="category text-uppercase py-3">
+            {{serie}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ComicsList",
+    props : {
+        serie: String,
+        thumb: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
