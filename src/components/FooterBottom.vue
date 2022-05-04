165758ffb73d27c0ec3f85d6f8e41f13acbb5c24
FraAccaBi <francescahuda@gmail.com>
2022-05-04 18:36:51 +0200

    backup commit

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row row-cols-2">
                 <div class="col"></div>
-                <div class="col">
+                <div class="col d-flex justify-content-evenly align-items-center">
                     <span>Follow us</span>
                     <div v-for="item in cards" :key="item.id">
                         <img :src="'./img/' + item.link" alt="">
@@ -58,14 +58,17 @@ export default {
 @import '~@/assets/scss/variables.scss';
 
 .container-fluid{
+    padding: 1.5rem;
     height: 80px;
     background-color:  $footer-bottom;
 }
 span{
     color: $dc-primary;
+    text-transform: uppercase;
+    font-weight: 900;
 }
 img{
-    height: 50px;
+    height: 30px;
 }
 
 
